c5f06c1e590a881a452b8e12f628fdbd8f0979d2
hirschharald <hirschharald@gmx.net>
2020-11-02 15:10:42 +0100

    Http Request with post

--- src/App.js
+++ src/App.js
@@ -1,85 +1,41 @@
+import { conforms } from "lodash";
 import React, { useState, useEffect } from "react";
-
-const useFetch = (req) => {
-  const { url, method, data } = req;
-  const [res, setRes] = useState({
-    data: null,
-    complete: false,
-    pending: false,
-    error: { error: false, message: "" }
-  });
-  useEffect(() => {
-    if (!url) return;
-    setRes({
-      data: null,
-      pending: true,
-      error: { error: false, message: "" },
-      complete: false
-    });
-
-    fetch(url, { method: method })
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          throw new Error("Fehler mit Status", response.status);
-        }
-      })
-      .then((data) => {
-        const fetch = async () => {
-          await setTimeout(() => {
-            console.log("timeout");
-            setRes({
-              data: data,
-              pending: false,
-              error: { error: false, message: "" },
-              complete: true
-            });
-          }, 5000);
-        };
-        fetch();
-      })
-      .catch((error) => {
-        console.log(error.message);
-        setRes({
-          data: null,
-          pending: false,
-          error: { error: true, message: error.message },
-          complete: false
-        });
-      });
-  }, [url, method]);
-  console.log(res);
-  const doFetch = () => {};
-  return [res, doFetch];
-};
-
-////////////////////////////////////////////////////////////
+import { doFetch } from "./usefetchHttp";
 
 export default function App() {
   const todosApi = "https://jsonplaceholder.typicode.com/todos";
-  const [count, setCount] = useState(1);
-  const [todo, setTodo] = useFetch({
-    method: "GET",
-    url: `${todosApi}/${count}`,
-    data: ""
-  });
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [todo, postNewTodo] = doFetch();
 
-  const createTodos = () => {
-    setTodo({
-      method: "GET",
-      url: `${todosApi}/${count}`,
-      data: ""
+  const createTodo = () => {
+    postNewTodo({
+      method: "POST",
+      url: todosApi,
+      body: { title: title, body: body, userId: 1 },
+      config: {
+        mode: "cors",
+        credentials: "include"
+      }
     });
   };
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Next Todo</button>
-      <h1>Todo {count}</h1>
-      <div className="error">{todo.error.error && todo.error.message}</div>
-      <div className="loading">{todo.pending && "Loading... "}</div>
-      <div>{todo.complete && todo.data.title}</div>
+      <h1>New Todo</h1>
+      <label>
+        Title:{" "}
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      </label>
+      <label>
+        Body: <input value={body} onChange={(e) => setBody(e.target.value)} />
+      </label>
+      <button onClick={createTodo}>Create Todo</button>
+      <div className="new-todo">
+        {(todo.pending && "Creating Todo...") ||
+          (todo.complete &&
+            `Todo with title ${todo.data.title} created with ID ${todo.data.id}`)}
+      </div>
     </div>
   );
 }
--- src/usefetchHttp.js
+++ src/usefetchHttp.js
@@ -1,40 +1,68 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = (req) => {
+const useFetch = (func) => {
   const [res, setRes] = useState({
     data: null,
     complete: false,
     pending: false,
-    error: false
+    error: { error: false, message: "" }
   });
-  console.log(req.url);
+  const [{ url, method, body, config }, setReq] = useState({});
+
   useEffect(() => {
+    if (!url) return;
     setRes({
       data: null,
       pending: true,
-      error: false,
+      error: { error: false, message: "" },
       complete: false
     });
-
-    fetch(req.url, { method: req.method })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setRes({
-          data: data,
-          pending: false,
-          error: true,
-          complete: true
-        });
+    console.log("fetch", method, url, JSON.stringify(body), config);
+    fetch(url, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      config
+    })
+      .then((response) => {
+        if (response.status === 200 || response.status === 201) {
+          return response.json();
+        } else {
+          throw new Error("Fehler mit Status", response.status);
+        }
+      })
+      .then((resp) => {
+        console.log("res", resp);
+        const fetch = async (resp) => {
+          await setRes({
+            data: resp,
+            pending: false,
+            error: { error: false, message: "" },
+            complete: true
+          });
+        };
+        fetch(resp);
       })
       .catch((error) => {
         setRes({
           data: null,
-          pending: true,
-          error: true,
+          pending: false,
+          error: { error: true, message: error.message },
           complete: false
         });
       });
-  }, [req.url]);
-  return res;
+  }, [url, method, body, config]);
+
+  return [res, (...args) => setReq(func(...args))];
+};
+
+export const doFetch = () => {
+  /* eslint-disable react-hooks/rules-of-hooks */
+  return useFetch((data) => ({
+    url: data.url,
+    method: data.method,
+    body: data.body,
+    config: data.config
+  }));
 };
